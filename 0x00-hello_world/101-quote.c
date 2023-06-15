e9e214759bd1691cbef93bd71fc3c06d1e1b47ff
stev_star <muruchistephen@gmail.com>
2023-06-15 09:00:47 -0700

    hello,world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <
+unistd.h
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * Return: 1
- */
+* main
+* Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-    return (1);
+write(2, "and that piece of art is useful
+\\" Dora Korpar ,
+2015 10 19 n", 59);
+return (1);
 }
