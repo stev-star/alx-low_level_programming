6422cac233dfb725ccb5432e421f7ff831a5ddfd
stev_star <muruchistephen@gmail.com>
2023-06-15 10:34:46 -0700

    hello, world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <
-unistd.h
+#include <stdio.h>
+#include <unistd.h>
 /**
-* main
-* Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
-*
-* Return: Always 1 (Success)
-*/
+ * main - prints the given information
+ *
+ * Return: 1
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful
-\\" Dora Korpar ,
-2015 10 19 n", 59);
+write(2,
+      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
 return (1);
 }
