25f35ad5f2dd8e57a0ea62e5d3de702e99233b1e
stev_star <muruchistephen@gmail.com>
2023-06-16 13:41:25 -0700

    if, else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/**
+
+ * main - Prints the alphabet.
+
+ *
+
+ * Return: 0
+
+ */
+
+int main(void)
+
+{
+
+        char alphab[26] = "abcdefghijklmnopqrstuvwxyz";
+
+        int i;
+
+
+        for (i = 0; i < 26; i++)
+
+        {
+
+                putchar(alphab[i]);
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
+}
