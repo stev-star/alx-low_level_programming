110800ed399c8c2caa5a58e4d98b9949c3e629b9
stev_star <muruchistephen@gmail.com>
2023-06-20 12:04:32 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,17 @@
 /**
  * print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
-        char letter;
-        int i;
+	int i;
+	char c;
 
-        while(i <= 10)
-        {
-                letter = 'a';
-                while(letter < 'z')
-                {
-                        _putchar(letter);
-                        letter++;
-                }
-                _putchar('\n');
-                i++;
-        }
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
