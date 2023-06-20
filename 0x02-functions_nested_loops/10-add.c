a1bdfa0cc01317e419ffcc868e0827322191c453
stev_star <muruchistephen@gmail.com>
2023-06-20 13:24:51 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - adds two numbers together.
+ * 
+ * Return: the sum of two numbers.
+ */
+int add(int i, int j)
+{i
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * 
+ * Return: absolute value of the input number.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
