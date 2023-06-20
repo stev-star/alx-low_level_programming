5298bb682954eec0ba9c034d820506a877c8c988
stev_star <muruchistephen@gmail.com>
2023-06-20 13:51:44 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,11 @@
   */
 int main(void)
 {
-        int i, res;
+		int i, res;
 
-        for (i = 0; i < 1024; i++)
-                if (i % 3 == 0 || i % 5 == 0)
-                        res += i;
-        printf("%d\n", res);
-        return (0);
+		for (i = 0; i < 1024; i++)
+				if (i % 3 == 0 || i % 5 == 0)
+						res += i;
+		printf("%d\n", res);
+		return (0);
 }
