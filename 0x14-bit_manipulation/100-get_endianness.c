b45767ad106ec9fb3c037284d49cec6dd70c7aa5
stev_star <muruchistephen@gmail.com>
2023-09-28 03:00:05 -0700

    updates

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianness - Check the endianess
+ *
+ * Return: void
+ */
+
+int get_endianness(void)
+{
+	int index;
+
+	index = 2;
+
+	if (index & 1)
+	{
+		return (0);
+	}
+
+	return (1);
+}
